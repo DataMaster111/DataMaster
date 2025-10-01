d4befd6521ef7a3786187d52bb0b02ec0b9fa22b
esmiraSar <esmirasar@yandex.ru>
2025-10-01 14:15:50 +0400

    transfer project

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, HTTPException
+from llama_cpp import Llama
+
 from api.v1 import router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.client = Llama(model_path="./model/YandexGPT-5-Lite-8B-instruct-Q4_K_M.gguf", verbose=False)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router=router)
-
-# @app.get("/test")
-# async def tets():
-#     return {"message": "success"}
-@app.get("/")
-def root():
-    return {"message": "Приветы, бэк работает))"}
-
